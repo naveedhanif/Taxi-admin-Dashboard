79d226326117009a50d113b4a269c6ca366ec5db
Naveed Hanif <contact.hanif@gmail.com>
2026-08-28 09:39:22 +0100

    Fix crash: missing useRef import; fix vercel.json breaking manifest.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import StepProgressIndicator from "./components/StepProgressIndicator";
 import BusinessDetailsStep from "./components/BusinessDetailsStep";
 import StripeConnectStep from "./components/StripeConnectStep";
